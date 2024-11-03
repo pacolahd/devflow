7002b645a8cbbdd8688f8405de6ad6f199f7408e
pacolahd <mbuntangwe@gmail.com>
2024-11-03 16:59:30 +0000

    Setup ESLint & Prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,9 @@
-// 'use client'
-import Image from "next/image";
-
-const Home = ()=> {
+const Home = () => {
   return (
-      <>
-        <h1 className='mt-3 text-3xl text-purple-800'>Welcome to Next.js</h1>
-          <p>jaja</p>
-          <p>papa</p>
-          aunty
-          unckle
-
-      </>
-
+    <>
+      <h1 className="mt-3 text-3xl text-purple-800 ">Welcome to Next.js</h1>
+    </>
   );
-}
+};
 
 export default Home;
