d829d27ca095593ad6bff4065a30207db2e1f3cd
pacolahd <mbuntangwe@gmail.com>
2025-01-25 18:49:06 +0000

    Setup Auth Layouts, Social Auth Form, Next Auth, OAuth with Google and GitHub Providers

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -6,7 +6,7 @@ import Theme from "@/components/navigation/navbar/Theme";
 
 const NavaBar = () => {
   return (
-    <nav className="flex-between background-light900_dark200 shaddow-light-300 dark:shaddow-none fixed z-50 w-full gap-5 p-6 sm:px-12 ">
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12 ">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/images/site-logo.svg"
---
+++ constants/routes.ts
@@ -0,0 +1,9 @@
+// Centralized place for all routes used in the app
+
+const ROUTES = {
+  HOME: "/",
+  SIGN_IN: "/sign-in",
+  SIGN_UP: "/sign-up",
+};
+
+export default ROUTES;
